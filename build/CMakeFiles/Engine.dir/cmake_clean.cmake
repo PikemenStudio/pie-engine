af52764173b1d33e2a162f81116146718e2480f0
Full-Hat <nikita3131228@gmail.com>
2023-09-01 10:48:16 -0700

    Setup vcpkg

---
+++ build/CMakeFiles/Engine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Engine.dir/Engine.cpp.o"
+  "CMakeFiles/Engine.dir/Engine.cpp.o.d"
+  "libEngine.a"
+  "libEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/full-hat/Documents/MyProjects/Engine
+# Build directory: /Users/full-hat/Documents/MyProjects/Engine/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
