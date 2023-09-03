81806adfb7cf04bcf3afe604e0a69fef70ff8424
Full-Hat <nikita3131228@gmail.com>
2023-09-03 00:44:27 -0700

    Add codestyle

---
+++ codestyle.md
@@ -0,0 +1,14 @@
+## pointers
+stars should be near name
+
+*char \*\*variable*
+
+const char *const variable
+
+## names
+
+classes
+
+class Parent
+
+functions 
--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,12 @@
 #include "gtest/gtest.h"
 
-int main()
+TEST(system, json)
 {
 
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
