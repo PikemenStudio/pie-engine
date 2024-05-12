929d19078efb8eae3d7eca54aec67b91ee1811a2
aneroid11 <a-e-v2003@yandex.by>
2024-05-12 20:58:56 +0300

    Generating some dummy floor

--- game/main.cpp
+++ game/main.cpp
@@ -3,10 +3,50 @@
 //
 
 #include <iostream>
+#include <vector>
+
+#include <noise/noise.h>
 
 #include <sound/facade/facade.hpp>
 
-int main()
+class Floor
+{
+public:
+  Floor(double StartX, double StepX, int PointsCount, const noise::module::Perlin& NoiseGen)
+  {
+    this->StartX = StartX;
+    this->StepX = StepX;
+    double CurrX = StartX;
+
+    while (PointsCount--)
+    {
+      WorldCoordsY.push_back(NoiseGen.GetValue(CurrX, 0, 0));
+      CurrX += StepX;
+    }
+  }
+
+  double getStartX() const
+  {
+    return StartX;
+  }
+
+  double getStepX() const
+  {
+    return StepX;
+  }
+
+  const std::vector<double>& getCoords() const
+  {
+    return WorldCoordsY;
+  }
+
+private:
+  double StartX = 0.0;
+  double StepX = 0.1;
+  std::vector<double> WorldCoordsY;
+};
+
+void soundTest()
 {
   SoundFacade Snd;
   auto SndId = Snd.ImplInstance.loadSound("../../game/resources/sound_test_mono.wav");
@@ -23,6 +63,21 @@ int main()
   while (Snd.ImplInstance.isPlaying(SndSrc))
   {
   }
+}
+
+int main()
+{
+  noise::module::Perlin NoiseGen;
+  NoiseGen.SetSeed(time(nullptr));
+  Floor FloorObj(0, 0.1, 10, NoiseGen);
+
+  const std::vector<double>& FloorCoords = FloorObj.getCoords();
+
+  for (auto Coord : FloorCoords)
+  {
+    std::cout << Coord << " ";
+  }
+  std::cout << "\n";
 
   return 0;
 }
