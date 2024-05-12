c3ae8e13d8f10e18a7425ff0d40cc86ff1d5497f
aneroid11 <a-e-v2003@yandex.by>
2024-05-12 17:04:54 +0300

    working game folder

--- game/main.cpp
+++ game/main.cpp
@@ -4,12 +4,25 @@
 
 #include <iostream>
 
-void func()
-{
-
-}
+#include <sound/facade/facade.hpp>
 
 int main()
 {
-    std::cout << "Game\n";
+  SoundFacade Snd;
+  auto SndId = Snd.ImplInstance.loadSound("../../game/resources/sound_test_mono.wav");
+  auto SndSrcParams = SoundStructs::SoundSourceParams {
+      .Pitch = 1.0f, .Gain = 1.0f,
+      .PosX = 0, .PosY = 0, .PosZ = 0,
+      .VelX = 0, .VelY = 0, .VelZ = 0,
+      .Looping = false,
+      .SndID = SndId
+  };
+  auto SndSrc = Snd.ImplInstance.createSoundSource(SndSrcParams);
+  Snd.ImplInstance.playSoundSource(SndSrc);
+
+  while (Snd.ImplInstance.isPlaying(SndSrc))
+  {
+  }
+
+  return 0;
 }
