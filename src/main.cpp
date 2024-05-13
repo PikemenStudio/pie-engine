39a9569fc28607648bf8054af634ec556b60d206
aneroid11 <a-e-v2003@yandex.by>
2024-05-13 23:31:48 +0300

    small changes

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,8 @@
 #include "../src/modules/graphics/facades/facade.hpp"
 #include "../src/modules/renderable_scene//facades/facade.hpp"
 #include "../src/modules/scene_manager/facades/facade.hpp"
-#include "glfw/glfw3.h"
+#include <GLFW/glfw3.h>
+
 #include <gtest/gtest.h>
 #include <iostream>
 
