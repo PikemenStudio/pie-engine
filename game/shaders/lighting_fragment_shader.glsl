1f1495edf572a82d4a19af14d8f40c9da5b0c9ed
aneroid11 <a-e-v2003@yandex.by>
2024-05-13 19:07:51 +0300

    Simple lighting

--- game/shaders/lighting_fragment_shader.glsl
+++ game/shaders/lighting_fragment_shader.glsl
@@ -1,3 +1,5 @@
+varying vec4 vert_pos;
+
 uniform sampler2D texture;
 
 void main()
@@ -5,6 +7,12 @@ void main()
     // lookup the pixel in the texture
     vec4 pixel = texture2D(texture, gl_TexCoord[0].xy);
 
+    vec2 delta = vert_pos.xy - vec2(0, 0); // light source in (0, 0)
+    float delta_len_squared = dot(delta, delta);
+//    float delta_len = length(delta);
+
+    float intensity = clamp(1.0 / (delta_len_squared + 1.1), 0.0, 1.0); // + 1.1 is important
+
     // multiply it by the color
-    gl_FragColor = gl_Color * pixel * 0.5;
+    gl_FragColor = gl_Color * pixel * intensity;
 }
--- game/shaders/lighting_vertex_shader.glsl
+++ game/shaders/lighting_vertex_shader.glsl
@@ -1,7 +1,11 @@
+varying vec4 vert_pos;
+
 void main()
 {
     // transform the vertex position
     gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+    // forward the vertex position
+    vert_pos = gl_Position;
 
     // transform the texture coordinates
     gl_TexCoord[0] = gl_TextureMatrix[0] * gl_MultiTexCoord0;
