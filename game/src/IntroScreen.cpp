a46523b1817a43f22484117e4cf678296fe5c397
aneroid11 <a-e-v2003@yandex.by>
2024-05-22 22:46:57 +0300

    Intro screen displaying text

--- game/src/IntroScreen.cpp
+++ game/src/IntroScreen.cpp
@@ -5,10 +5,49 @@
 #include "IntroScreen.h"
 
 #include <iostream>
+#include <loguru.hpp>
 
 IntroScreen::IntroScreen(sf::RenderWindow* Win)
 : Window(Win)
 {
+  if (!TextFont.loadFromFile("../../game/resources/Fonts/Ostrovsky-Bold.otf"))
+  {
+    LOG_F(ERROR, "Cannot load font!");
+  }
+
+  addLine(L"Меня зовут Вито Скалетта.");
+  addLine(L"Используйте стрелки для перемещения.");
+  addLine(L"V - снизить/увеличить яркость лампы.");
+  addLine(L"E - пройти через проход.");
+}
+
+void IntroScreen::addLine(const std::wstring& LineText)
+{
+  std::unique_ptr<sf::Text> Text = std::make_unique<sf::Text>();
+  Text->setFont(TextFont);
+  Text->setString(LineText);
+  // in pixels, not points!
+  Text->setCharacterSize(30);
+  const sf::Color TextColor(200, 200, 200);
+  Text->setFillColor(TextColor);
+
+  float TextPosX = 10;
+  float TextPosY;
+
+  if (Lines.empty())
+  {
+    TextPosY = 10;
+  }
+  else
+  {
+    TextPosY = Lines.back()->getPosition().y +
+               Lines.back()->getLineSpacing() +
+               Lines.back()->getCharacterSize();
+  }
+
+  Text->setPosition(TextPosX, TextPosY);
+
+  Lines.push_back(std::move(Text));
 }
 
 void IntroScreen::handleUserInput()
@@ -25,9 +64,18 @@ void IntroScreen::handleUserInput()
   }
 }
 
+void IntroScreen::drawText()
+{
+  for (const auto& Line : Lines)
+  {
+    Window->draw(*Line);
+  }
+}
+
 void IntroScreen::renderScreen()
 {
-  Window->clear(sf::Color(40, 40, 40));
+  Window->clear(sf::Color(30, 30, 30));
+  drawText();
   Window->display();
 }
 
--- game/src/IntroScreen.h
+++ game/src/IntroScreen.h
@@ -7,18 +7,28 @@
 
 #include <SFML/Graphics.hpp>
 
+#include <memory>
+#include <vector>
+
 class IntroScreen
 {
 public:
   IntroScreen(sf::RenderWindow* Win);
 
+  void addLine(const std::wstring& LineText);
+
   void handleUserInput();
+  void drawText();
   void renderScreen();
   void run();
 
 private:
   sf::RenderWindow* Window;
+
+  sf::Font TextFont;
   bool IsRunning = true;
+
+  std::vector<std::unique_ptr<sf::Text>> Lines;
 };
 
 #endif // ENGINE_INTROSCREEN_H
