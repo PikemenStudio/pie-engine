ffa8d52e2fd6a8e567894295c38238b0891d63be
aneroid11 <a-e-v2003@yandex.by>
2024-05-24 15:32:36 +0300

    some text in intro screen

--- game/src/IntroScreen.cpp
+++ game/src/IntroScreen.cpp
@@ -15,10 +15,14 @@ IntroScreen::IntroScreen(sf::RenderWindow* Win)
     LOG_F(ERROR, "Cannot load font!");
   }
 
-  addLine(L"Меня зовут Вито Скалетта.");
+  addLine(L"Меня зовут Вито Скалетта. Я родился на Сицилии в 25 году.");
+  addLine(L"Я мало что помню о тех временах, разве что жизнь была тяжёлой.");
+  addLine(L"\t");
   addLine(L"Используйте стрелки для перемещения.");
   addLine(L"V - снизить/увеличить яркость лампы.");
   addLine(L"E - пройти через проход.");
+  addLine(L"Ваша задача - найти выход.");
+  addLine(L"Нажмите любую клавишу...");
 }
 
 void IntroScreen::addLine(const std::wstring& LineText)
